2e636491f7d58bae443903f03467fbc1657cdc22
robbyari <robbyariw23@gmail.com>
2023-05-17 13:31:51 +0700

    Penambahan ShoesItem.kt

---
+++ app/src/main/java/com/robbyari/tokosepatu/ui/components/ShoesItem.kt
@@ -0,0 +1,119 @@
+package com.robbyari.tokosepatu.ui.components
+
+import android.content.res.Configuration
+import android.provider.CalendarContract
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.robbyari.tokosepatu.R
+import com.robbyari.tokosepatu.ui.theme.TokoSepatuTheme
+
+@Composable
+fun ShoesItem(
+    image: Int,
+    title: String,
+    price: String,
+    rating: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Box(
+            modifier = modifier
+                .size(150.dp, 150.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .background(color = Color.DarkGray),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(image),
+                contentDescription = null,
+                modifier = modifier
+                    .size(130.dp)
+                    .rotate(-15f)
+                    .offset(-8.dp)
+            )
+            TextButton(
+                onClick = {},
+                modifier = modifier.offset(-50.dp, -50.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.FavoriteBorder,
+                    contentDescription = null,
+                    modifier = modifier.size(30.dp),
+                    tint = Color.White
+                )
+            }
+        }
+        Row(
+            modifier = modifier.offset(-50.dp),
+        ) {
+            Icon(
+                imageVector = Icons.Default.Star,
+                contentDescription = null,
+                modifier = modifier.size(20.dp),
+                tint = Color.Yellow
+            )
+            Text(
+                text = rating,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = modifier
+
+            )
+        }
+        Text(
+            text = price,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.offset(-20.dp)
+        )
+        Text(
+            text = title,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.size(140.dp, 20.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = false, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun ShoesItemPreview() {
+    TokoSepatuTheme {
+        Surface {
+            ShoesItem(image = R.drawable.air_force_1_sculpt_shoes, title = "Air Force 1 Sculp Shoes", price = "Rp 2.450.000", rating = "4.5")
+        }
+    }
+}
